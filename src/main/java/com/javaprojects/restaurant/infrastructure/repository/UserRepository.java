91ca4b30b9c67076e0f18e00aa8f8f682478d538
felipsants <165330370+felipsants@users.noreply.github.com>
2024-12-11 19:56:54 -0300

    Created findByEmail function

--- src/main/java/com/javaprojects/restaurant/infrastructure/repository/UserRepository.java
+++ src/main/java/com/javaprojects/restaurant/infrastructure/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.javaprojects.restaurant.infrastructure.repository;
 import com.javaprojects.restaurant.infrastructure.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
 }
