c3878f5e1df8dc4b62b2da0761a7f260a9134c76
felipsants <165330370+felipsants@users.noreply.github.com>
2024-12-11 19:57:46 -0300

    Created createUser, getUserById and getUserByEmail

---
+++ src/main/java/com/javaprojects/restaurant/service/UserService.java
@@ -0,0 +1,25 @@
+package com.javaprojects.restaurant.service;
+
+import com.javaprojects.restaurant.infrastructure.entity.UserEntity;
+import com.javaprojects.restaurant.infrastructure.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity createUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+    public UserEntity getUserById(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+
+    public UserEntity getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+}
